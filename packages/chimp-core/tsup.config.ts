67a153887e8e7a67e45d3d8245a71fcbff32938f
Mark Rabey <mark@markrabey.com>
2025-06-19 13:01:25 -0400

    chore(core): update build config

--- packages/chimp-core/tsup.config.ts
+++ packages/chimp-core/tsup.config.ts
@@ -1,11 +1,27 @@
-export default {
-  entry: ['src/**/*.ts'],
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: [
+    'src/index.ts',
+    'src/utils/index.ts',
+    'src/utils/changelog/index.ts',
+    'src/utils/logger.ts',
+    'src/utils/openai.ts',
+    'src/config.ts',
+    'src/env.ts',
+    'src/types/config.ts',
+    'src/cli/index.ts',
+    'src/cli/config.ts',
+    'src/cli/init.ts',
+    'src/cli/addChimpConfigCommand.ts',
+    'src/cli/changelog.ts',
+  ],
   dts: true,
   outDir: 'dist',
   clean: true,
-  splitting: false,
   format: ['esm', 'cjs'],
-  shims: false,
   sourcemap: true,
+  shims: false,
+  splitting: false,
   onSuccess: 'echo ✅ Build complete',
-};
+});
